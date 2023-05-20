3e445bfce3c6742ace051518cca7662660b6d68c
Test USer <testex@abc.com>
2023-05-21 00:10:10 +0500

    Add project files.

---
+++ GreyWAS/Server/Data/GreyWASServerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GreyWAS.Shared;
+
+namespace GreyWAS.Server.Data
+{
+    public class GreyWASServerContext : DbContext
+    {
+        public GreyWASServerContext (DbContextOptions<GreyWASServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GreyWAS.Shared.Student> Student { get; set; } = default!;
+    }
+}
---
+++ GreyWAS/Shared/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyWAS.Shared
+{
+    public class Student
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public decimal Totalmarks { get; set; }
+
+    }
+}
